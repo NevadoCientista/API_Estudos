d31f17d43bd81d3da4a6101a7fc0904caebd66e5
NevadoCientista <44269021+NevadoCientista@users.noreply.github.com>
2020-07-23 05:39:00 -0300

    Tentativa de resolução 1

--- boilerplate-project-timestamp/server.js
+++ boilerplate-project-timestamp/server.js
@@ -30,3 +30,26 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// Tentativa 1
+// 1
+app.get( "/api/timestamp/:date_string?", (req, res) => {
+  const dateInputed = req.params.date_string;
+
+  // 4
+  try{
+    const dateValid = new Date( dateInputed );
+    res.json({ unix: dateValid.getTime(), utc: dateValid.toUTCString() });
+  }catch{ 
+    // 5
+    res.json({ "error": "Invalid Date" });
+  } 
+});
+
+// 3
+app.get( "/api/timestamp/" ,  (req, res) => {
+  const nowDate = new Date();
+  res.json( { unix: nowDate.getTime(), utc: nowDate.toUTCString() } );
+});
+
+
